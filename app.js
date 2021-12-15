7cd56d190ac830c5c6f4f7abe97ac61ae3cd864b
martinchiu <martin87713@gmail.com>
2021-12-15 10:54:14 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 // 引入第三方套件
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 // 引入自建套件
 require('./config/mongoose')
 
+// 啟動 express
 const app = express()
 
+// 設定連接阜
 const PORT = 3000
 
+// 設定模板引擎
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs'}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
