aa97a47486743f80ae376e1fd0d2bc2d4193c30d
martinchiu <martin87713@gmail.com>
2021-12-15 09:50:01 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,14 @@
+// 引入各種套件
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
