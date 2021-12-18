e6acb65bf254e402793f197063424d0f8e45f5e9
martinchiu <martin87713@gmail.com>
2021-12-18 11:17:17 +0800

    feat: add copy function

--- utility/formCheck.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('#url-form')
-const inputValue = document.querySelector('#form-input').value
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  event.stopPropagation()
-  console.log(inputValue)
-})
